ebac122f991c47fcbe9d4831d6744f7b074ba60d
Luca <luca.nunzi@colossusbets.com>
2021-03-24 10:32:01 +0000

    .

--- main.go
+++ main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"fmt"
+	"github.com/luca0x333/go-algos/binary_search"
 	"github.com/luca0x333/go-algos/quicksort"
 )
 
 func main() {
 	xi := []int{1, 2, 4, 3, 2, 45, 65, 32, 143, 67, 32, 23}
 
-	// fmt.Println(binary_search.BinarySearchSwitch(xi, 32))
-	// fmt.Println(binary_search.BinarySearch(xi, 32))
-	// fmt.Println(binary_search.SimpleSearch(xi, 32))
+	fmt.Println(binary_search.BinarySearchSwitch(xi, 32))
+	fmt.Println(binary_search.BinarySearch(xi, 32))
+	fmt.Println(binary_search.SimpleSearch(xi, 32))
 
 	fmt.Println(quicksort.QuickSort(xi))
 }
